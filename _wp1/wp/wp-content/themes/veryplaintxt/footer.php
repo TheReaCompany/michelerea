a908c68172f34701f6dd0494f52c095f40b6514e
Alexander Rea <alexander@alexanderrea.com>
2015-05-25 17:09:30 -0400

    First commit

---
+++ _wp1/wp/wp-content/themes/veryplaintxt/footer.php
@@ -0,0 +1,10 @@
+	<div id="footer">
+		<span id="copyright"><a href="mailto:michele@reaproductions.com">michele@reaproductions.com</a></span>
+	</div><!-- #footer -->
+
+<?php wp_footer() // Do not remove; helps plugins work ?>
+
+</div><!-- #wrapper -->
+
+</body><!-- end trasmission -->
+</html>
---
+++ ndxz-studio/config/config.php
@@ -0,0 +1,9 @@
+<?php  if (!defined('SITE')) exit('No direct script access allowed');
+
+$indx['db'] 		= 'micheler_websit3';
+$indx['user'] 		= 'micheler_dbus3r';
+$indx['pass'] 		= 'J;}b*V5uwe*m';
+$indx['host'] 		= 'localhost';
+$indx['sql']		= 'mysql';
+
+?>
