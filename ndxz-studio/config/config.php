aa653bab895f4a1a04125958849366d440da6eaf
Alexander Rea <alexander@alexanderrea.com>
2015-08-16 16:02:09 -0400

    DB update

--- ndxz-studio/config/config.php
+++ ndxz-studio/config/config.php
@@ -3,7 +3,7 @@
 $indx['db'] 		= 'micheler_websit3';
 $indx['user'] 		= 'micheler_dbus3r';
 $indx['pass'] 		= 'J;}b*V5uwe*m';
-$indx['host'] 		= 'localhost';
+$indx['host'] 		= 'trc-db-1.cskowdozwezg.us-east-1.rds.amazonaws.com';
 $indx['sql']		= 'mysql';
 
 ?>
